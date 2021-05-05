1c1c5a08365c88688eb98bf59f06b7dbb79ae801
Daniel Sanz Mayo <dasanz04@ucm.es>
2021-05-05 20:19:23 +0200

    Vista Producto en CSS + imagenes y descripciones de productos buenas

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,13 +4,13 @@ INSERT INTO Usuario VALUES (3, 'Casado Molinero' , 1, 'Alvaro', '{bcrypt}$2a$10$
 INSERT INTO Usuario VALUES (4, 'Ivan Susana' , 1, 'Jin', '{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95JhcOaG23fHRpDoZIJrsF2cCff9xEHTTdK1u', 'USER',1000, 'Ivanchino07');
 INSERT INTO Usuario VALUES (5, 'Peng Zhou' , 1, 'JinTao', '{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95JhcOaG23fHRpDoZIJrsF2cCff9xEHTTdK1u', 'ADMIN',1000, 'TheRealJin');
 
-INSERT INTO Producto VALUES (1, 'Zapatillas', 'Nuevas zapatillas colaboracion con Kanye West.', 'Adidas Yeezy 700', '42.5');
-INSERT INTO Producto VALUES (2, 'Zapatillas', 'Nuevas zapatillas con un disenio rompedor', 'Adidas Yeezy 500', '45');
-INSERT INTO Producto VALUES (3, 'Zapatillas', 'Colaboración entre BAPE y adidas', 'Adidas NMD Bape Camo', '39');
-INSERT INTO Producto VALUES (4, 'Streetwear', 'Colaboracion entre las conocidas marcas de Supreme y The North Face', 'TNF Mountain Parka', 'L');
-INSERT INTO Producto VALUES (5, 'Accesorios', 'Oso de peluche hecho en alemania', 'Supreme Teddy Bear', 'Unica');
-INSERT INTO Producto VALUES (6, 'Relojes', 'Reloj ROLEX iconico y super limitado, solo 500 unidades en todo el mundo', 'Rolex GTM-Master II "Batman"', 'Unica');
-INSERT INTO Producto VALUES (7, 'Electronica', 'Consola de uktima generacion', 'Sony Play Station 5', '1 TB');
+INSERT INTO Producto VALUES (1, 'Zapatillas', 'Nuevas zapatillas colaboracion entre Kanye West y Adidas. Presentan un upper con diferentes materiales como primeknit azul, mesh gris y neopreno negro unidas por una estructura de plastico translucida que brilla en la oscuridad.', 'Adidas Yeezy 700', '42.5');
+INSERT INTO Producto VALUES (2, 'Zapatillas', 'Nuevas zapatillas creadas por el rapero Kanye West en colaboracion con la marca alemana. Presenta una mediasuela de BOOST, una tecnologia de adidas para mejorar la comodidad y un upper primeknit que llena zapatilla de diferentes texturas.', 'Adidas Yeezy Boost 350', '45');
+INSERT INTO Producto VALUES (3, 'Zapatillas', 'Colaboracion entre Nike y Jordan plasmada en la silueta de la Jordan 1 High OG. Esta zapatilla lanzada en 2019 presenta un upper de cuero con unos detalles en ante desgastado de color azul-verdoso. Ademas contienen un segundo par de cordones.', 'Air Jordan 1 "Turbo Green"', '39');
+INSERT INTO Producto VALUES (4, 'Streetwear', 'Sudadera de la conocida marca de Streetwear perneteciente a la temporada FW16. En esta temporada Supreme lanzo al mercado esta sudadera en este color verde oliva, junto con 5 colores mas. Este es uno de los colores mas deseados de la temporada.', 'Supreme Box Logo Hoodie "Olive"', 'L');
+INSERT INTO Producto VALUES (5, 'Accesorios', 'Zeppelin inflable de Supreme. Presenta una cuerda larga para poder colgarlo y utilizarlo como decoracion. Perteneciente a la temporada Fall-Winter 17, donde supreme saco un gran numero de accesorios nunca vistos. Hazte con el tuyo ya!', 'Supreme Inflatable Blimp', 'Unica');
+INSERT INTO Producto VALUES (6, 'Relojes', 'Reloj de la lujosa marca ROLEX. Solo existen 500 unidades de este lujoso reloj en todo el mundo. En nuestra tienda, puedes obtener este Rolex GTM nuevo y original. Presenta una construcion de acero inoxidable y una esfera negra elegante.', 'Rolex GTM-Master II "Batman"', 'Unica');
+INSERT INTO Producto VALUES (7, 'Electronica', 'Consola de ultima generacion de Sony que esta arrasando en el mercado. La consola tiene un color blanco e inluye un mando de color blanco a juego. Con una capacidad de 1TB tienes capacidad para almacenar todos los juegos que necesites!', 'Sony Play Station 5', '1 TB');
 
 
 
